a1fbba8f3a35897fd81b59f522c587a95d38475c
amurrai <andre.murrai@gmail.com>
2023-11-17 21:52:52 -0500

    add message function

--- input.js
+++ input.js
@@ -16,6 +16,9 @@ const handleUserInput = function (key) {
   if (key === 'd') {
     connection.write("Move: right");
   }
+  if (key === 't') {
+    connection.write("Say: Let's go!");
+  }
 };
 
 const setupInput = (conn) => {
