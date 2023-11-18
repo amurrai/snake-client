a41cceb68b0d6d7f075e63d67908a1216503e1de
amurrai <andre.murrai@gmail.com>
2023-11-17 19:24:03 -0500

    add client function to send initials on connect

--- client.js
+++ client.js
@@ -10,9 +10,13 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  conn.on("connect", () => {
+    conn.write("Name: AAA");
+  });
+
   conn.on("data", (data) => {
     console.log(data);
-  })
+  });
 
   return conn;
 };
